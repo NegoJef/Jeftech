31edf3b34a7c25ebfb56d5c3df015a69215611b4
NegoJef <4571843@gmail.com>
2024-07-08 14:49:32 -0300

    commit 4

--- main.py
+++ main.py
@@ -1,4 +1,20 @@
 #Jeftech
+#Função pra criar novo produto
+class produto:
+    def __init__(self,nome,valor,):
+        self.nome = nome
+        self.valor = valor
+    def __repr__(self):
+        return f"{self.nome}, Valor={self.valor}"
+    
+def cadastrar_produto(nome,valor,):
+    return produto(nome,valor)
+
+
+
+
+
+
 
 #-----Menu-----#
 while True:
@@ -6,6 +22,10 @@ while True:
 
     if menu == 1:
         print("Cadastro de produtos")
+        nome1 = str(input("Digite o nome do produto:"))
+        valor1 = float(input("Digite o valor do produto:"))
+        produto1 = cadastrar_produto(nome1,valor1)
+        print(produto1)
     elif menu == 2:
         print("Compra de produtos")
     elif menu == 3:
