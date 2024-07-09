39588963fb8e9289c481d8ad5187262e66ddf8dd
NegoJef <4571843@gmail.com>
2024-07-08 22:50:10 -0300

    commit 5

--- main.py
+++ main.py
@@ -1,38 +1,49 @@
 #Jeftech
 #Função pra criar novo produto
-class produto:
-    def __init__(self,nome,valor,):
-        self.nome = nome
-        self.valor = valor
-    def __repr__(self):
-        return f"{self.nome}, Valor={self.valor}"
-    
-def cadastrar_produto(nome,valor,):
-    return produto(nome,valor)
+def cadastrar_produto(estoque):
+    nome_produto = input("Digite o nome do produto: ")
+    quantidade = int(0)
+    valor = float(0.0)
+
+    estoque[nome_produto] = {"Quantidade": quantidade, "Valor": valor}
+    print(f"Produto {nome_produto} cadastrado com sucesso!")
+    return estoque
 
 
+def mostrar_estoque(estoque):
+    print("\nEstoque atual:")
+    for produto, dados in estoque.items():
+        print(f"Produto: {produto}, Quantidade: {dados['Quantidade']}, Valor: R$ {dados['Valor']:.2f}")
+    print()
 
+estoque = {}
 
-
-
-
-#-----Menu-----#
 while True:
-    menu = int(input("Digite a opção que deseja(de 1 a 5):"))
+    print("Menu:")
+    print("1. Cadastrar produto")
+    print("2. Alterar produto")
+    print("3. Mostrar estoque")
+    print("4. Prejuízo/Despesas")
+    print("5. Sair")
+
+    menu = int(input("Digite a opção que deseja (de 1 a 5): "))
 
     if menu == 1:
-        print("Cadastro de produtos")
-        nome1 = str(input("Digite o nome do produto:"))
-        valor1 = float(input("Digite o valor do produto:"))
-        produto1 = cadastrar_produto(nome1,valor1)
-        print(produto1)
+        while True:
+            cad = int(input("Deseja cadastrar um produto? \n1-> Sim\n2-> Não\n: "))
+            if cad == 1:
+                cadastrar_produto(estoque)
+            elif cad == 2:
+                break
+            else:
+                print("Digite um valor válido.\n **********************************")
     elif menu == 2:
-        print("Compra de produtos")
+        print("Comprar produtos")
     elif menu == 3:
-        print("Venda de produtos")
+        mostrar_estoque(estoque)
     elif menu == 4:
-        print("Prejuizo/Despesas")
+        print("Prejuízo/Despesas")
     elif menu == 5:
         break
     else:
-        print("Digite um valor válido")
+        print("Digite um valor válido.")
